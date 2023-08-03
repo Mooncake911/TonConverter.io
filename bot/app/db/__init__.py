9d062c02a5a197360f95a3c640954d14d2eb7c25
Vadim Shaidurov <90641016+Mooncake911@users.noreply.github.com>
2023-08-03 18:19:24 +0500

    Delete bot/app directory

--- bot/app/db/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = ["BaseModel", "create_engine", "get_session_maker", "proceed_schemas"]
-
-from .base import BaseModel
-from .engine import create_engine, get_session_maker, proceed_schemas
-from .postgre_sql import User
